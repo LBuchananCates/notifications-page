407650dd131b4f0dfe9b7fb23c84a9d729d1ee99
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2023-02-23 17:27:29 -0500

    completes all css styles for both mobile and desktop

--- notifications.js
+++ notifications.js
@@ -4,7 +4,9 @@ markAllAsReadBtn.addEventListener("click", function () {
   const notifContainer = document.getElementsByClassName(
     "notification-container"
   );
-  notifContainer[(0, 1, 2)].style.backgroundColor = "white";
+  notifContainer[0].style.backgroundColor = "white";
 });
 
 // notification number starts at 3, resets to 0 when click button
+
+// if notification-container bg color = light blue, when click button remove background color
