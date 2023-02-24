4d8e4fb31730171e7a3c2cf79e3c090ac44b1ba7
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2023-02-24 15:37:31 -0500

    red dot and background color changes, but only on first notification-container

--- notifications.js
+++ notifications.js
@@ -4,14 +4,16 @@ const markAllAsReadBtn = document.querySelector(".mark-all-as-read");
 // create event listener for mark all as read button
 markAllAsReadBtn.addEventListener("click", function () {
   // grab divs with class of unread
-  const unreadNotifs = document.querySelectorAll(".unread");
+  const unreadNotifs = document.querySelector(".unread");
 
   // turn unread divs background white
-  //   unreadNotifs.style.backgroundColor = "white";
+  unreadNotifs.style.backgroundColor = "white";
 
   // turn notifications from 3 to 0 DONE
   const currentNotifNum = document.querySelector(".notification-number");
   currentNotifNum.textContent = "0";
 
   // display none for red-dot
+  const notificationDot = document.querySelector(".red-dot");
+  notificationDot.style.display = "none"; // got this working for the first container, but with queryselectorall, doesn't work
 });
