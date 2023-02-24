bd4df476e53d4e34b41103a089d4db362ee1176b
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2023-02-24 15:34:01 -0500

    adds functionality of notification numbers when button is clicked

--- notifications.js
+++ notifications.js
@@ -1,12 +1,17 @@
-// create event listener for mark all as read
+// grab mark all as read button
 const markAllAsReadBtn = document.querySelector(".mark-all-as-read");
+
+// create event listener for mark all as read button
 markAllAsReadBtn.addEventListener("click", function () {
-  const notifContainer = document.getElementsByClassName(
-    "notification-container"
-  );
-  notifContainer[0].style.backgroundColor = "white";
+  // grab divs with class of unread
+  const unreadNotifs = document.querySelectorAll(".unread");
+
+  // turn unread divs background white
+  //   unreadNotifs.style.backgroundColor = "white";
+
+  // turn notifications from 3 to 0 DONE
+  const currentNotifNum = document.querySelector(".notification-number");
+  currentNotifNum.textContent = "0";
+
+  // display none for red-dot
 });
-
-// notification number starts at 3, resets to 0 when click button
-
-// if notification-container bg color = light blue, when click button remove background color
