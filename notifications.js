096cfdc039e8e800ed9f36783ae6fed8bff13118
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2023-02-27 10:25:13 -0500

    successfully adds and removes classes for all unread notifications

--- notifications.js
+++ notifications.js
@@ -4,17 +4,21 @@ const markAllAsReadBtn = document.querySelector(".mark-all-as-read");
 // create event listener for mark all as read button
 markAllAsReadBtn.addEventListener("click", function () {
   // grab divs with class of unread [works only for ONE container]
-  const unreadNotifs = document.querySelector("div.unread");
+  const unreadNotifs = document.querySelectorAll("div.unread");
+  for (notif of unreadNotifs) {
+    // add and remove classes
+    notif.classList.add("read");
+    notif.classList.remove("unread");
+  }
 
-  // turn unread divs background white WORKA ON ONE
-  unreadNotifs.classList.add("read");
-  unreadNotifs.classList.remove("unread");
+  // display none for red-dot
+  const notificationDot = document.querySelectorAll(".red-dot");
+  for (const notif of notificationDot) {
+    notif.classList.remove("red-dot");
+    notif.style.display = "none";
+  }
 
   // turn notifications from 3 to 0 DONE
   const currentNotifNum = document.querySelector(".notification-number");
   currentNotifNum.textContent = "0";
-
-  // display none for red-dot
-  const notificationDot = document.querySelector(".red-dot");
-  notificationDot.style.display = "none"; // got this working for the first container, but with queryselectorall, doesn't work
 });
